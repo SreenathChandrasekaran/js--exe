9cb23cb709de786a16ec0fa762a983cd18867f5c
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-10 12:34:58 +0530

    2nd Program 7th exercise

--- 02_JS_variables/Ch2_pgm_07.js
+++ 02_JS_variables/Ch2_pgm_07.js
@@ -2,8 +2,10 @@
 
 var playerName = "Kandra";
 var locationName = "The Dungeon of Doom";
+var health = "50";
 
 console.log(playerName + " is in " + locationName);
+console.log(playerName+" has health "+health+" and is in "+locationName);
 
 
 
