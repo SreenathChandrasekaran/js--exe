c44c24ddde118f4e5141944687b9598e9fcac7ee
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-10 18:32:24 +0530

    3rd Program 12th exercise

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,6 +8,18 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+console.log(post.id + post.title + post.author + post.created + post.body);
+
+var post2 = {
+	id : 2,
+	title : "Harrypotter",
+	author : "J.K.Rowling",
+	created : "1999-05-20",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+}
+
+console.log(post2.id + post2.title + post2.author + post2.created + post2.body);
+
 
 
 /* Further Adventures
