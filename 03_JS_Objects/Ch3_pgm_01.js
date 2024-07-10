9c5e09ee97c4808e79654a8819d6e8da36533fc0
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-10 14:32:37 +0530

    3rd Program 1st exercise

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -8,6 +8,29 @@ bookAuthor = "J. R. R. Tolkien";
 
 console.log(bookTitle + " by " + bookAuthor);
 
+book2 = "Wings Of Fire";
+author2 = "Dr.A.P.J Abdul Kalam";
+
+console.log(book2+" by "+author2);
+
+book3 = "Mahabharat";
+author3 = "Veda Vyasa";
+
+console.log(book3+" by "+author3);
+
+for(i=0;i<=10;i++){
+    book4 = "Siruvar Malar"
+    author4 = "Thinamalar"
+    console.log(book4+" by "+author4)
+}
+
+for(i=0;i<=100;i++){
+    book5 = "Siruvar Malar"
+    author5 = "Thinamalar"
+    console.log(book5+" by "+author5)
+}
+
+
 
 
 /* Further Adventures
