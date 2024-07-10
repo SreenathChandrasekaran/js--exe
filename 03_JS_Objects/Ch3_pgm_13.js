855c1726be677f476c68cd1d27878f60498a4687
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-10 18:36:23 +0530

    3rd Program 13th exercise

--- 03_JS_Objects/Ch3_pgm_13.js
+++ 03_JS_Objects/Ch3_pgm_13.js
@@ -1,15 +1,20 @@
 // Location for a weather app
 
-var location = {
+var geolocation = {
     "city"      : "San Francisco",
     "state"     : "CA",
     "country"   : "US",
     "zip"       : "94101",
     "latitude"  : 37.775,
     "longitude" : -122.418,
-    "elevation" : 47.000
+    "elevation" : 47.000,
   };
   
+  
+
+  console.log(geolocation.city);
+
+  console.log(geolocation);
   
   
   /* Further Adventures
