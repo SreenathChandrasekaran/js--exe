60bc4212642a1b185aaceec7a0d5dbbcf3638eb5
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-14 08:09:37 +0530

    7th program 3rd exercise

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -25,7 +25,30 @@ planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
 console.log(getPlanetInfo(planet1));
 console.log(getPlanetInfo(planet2));
 
+var cars;
+var car1;
+var car2;
+var getcarinfo;
 
+cars = function (name,company,price,model,type) {
+    return {
+        name: name,
+        company: company,
+        price: price,
+        model: model,
+        type: type
+    }
+};
+
+getcarinfo = function(cars) {
+    return cars.name.toUpperCase() + " from " + cars.company + " is $ " + cars.price + " is a " + cars.type;
+};
+
+car1 = cars("Virtus","Volkswagon",1500000,2022,"Sedan");
+car2 = cars("Rapid","Skoda",1600000,2022,"Sedan");
+
+console.log(getcarinfo(car1));
+console.log(getcarinfo(car2));
 
 /* Further Adventures
  *
