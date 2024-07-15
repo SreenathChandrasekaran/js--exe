5d10f8bbf88fc87590f4ebceea7fc921bb998943
SreenathChandrasekaran <srinathc2002@gmail.com>
2024-07-15 14:53:31 +0530

    8th program 5th exercise

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -6,7 +6,9 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
     "Tuesday",
     "Wednesday",
     "Thursday",
-    "Friday"
+    "Friday",
+    "Saturday",
+    "Sunday"
   ];
 	var index = dayInWeek - 1;
 	var visitorReport;
@@ -19,11 +21,29 @@ var getVisitorReport = function (visitorArray, dayInWeek) {
 	return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
+var visitors = [ 354, 132, 210, 221, 481, 321, 585 ];
 
 var report = getVisitorReport(visitors, 2);
 
 console.log(report);
+
+report = getVisitorReport(visitors, 5);
+
+console.log(report);
+
+var week1 = [281, 330, 350, 280, 490, 510, 585];
+var week2 = [321, 291, 356, 400, 456, 500, 590];
+var week3 = [239, 330, 341, 390, 490, 480, 530];
+var week4 = [250, 290, 285, 275, 410, 469, 525];
+var month = [week1, week2, week3, week4];
+
+var monthinfo = function(month, week,dayInWeek ){
+  var weekinfo = week - 1;
+  var monthdetails = month[weekinfo];
+  return getVisitorReport(monthdetails,dayInWeek);
+};
+
+console.log(monthinfo(month,4,5));
 
 
 
